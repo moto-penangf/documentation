b15c96271dc64be15330c3ef025a2b054097107e
Shomy 🍂 <61943525+shomykohai@users.noreply.github.com>
2025-01-18 19:50:45 +0100

    Fix github urls

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -86,7 +86,7 @@ const config: Config = {
         },
         // {to: '/blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/moto-penangf/documentation',
           label: 'GitHub',
           position: 'right',
         },
@@ -122,7 +122,7 @@ const config: Config = {
           items: [
             {
               label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
+              href: 'https://github.com/orgs/moto-penangf/',
             },
           ],
         },
