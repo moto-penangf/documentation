2397ca020d91f2e15ee3992f2272e82fe0a0d384
Shomy 🍂 <61943525+shomykohai@users.noreply.github.com>
2025-01-18 19:53:16 +0100

    Use correct link for editing page

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -40,7 +40,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/moto-penangf/documentation/',
+            'https://github.com/moto-penangf/documentation/edit/',
         },
         blog: {
           showReadingTime: true,
@@ -50,8 +50,6 @@ const config: Config = {
           },
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/moto-penangf/documentation/',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
