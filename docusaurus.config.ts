f5183e87bc6e82db32d4cbcd035f6041b3e30bc1
Diablo <80642969+progzone122@users.noreply.github.com>
2025-01-25 22:50:01 +0200

    Update docusaurus.config.ts
    update domain

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,10 +10,10 @@ const config: Config = {
   // favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://moto-penangf.github.io',
+  url: 'https://penangf.fuckyoumoto.xyz',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/documentation/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
