2458a1d8214b4a0c9e08d4f3710690b4e1e76aa4
diablo <progzone122@gmail.com>
2025-02-02 01:23:29 +0000

    update gsi roms list

--- docs/gsi_roms/gsi_roms.js
+++ docs/gsi_roms/gsi_roms.js
@@ -1,12 +1,17 @@
 export const a14 = {
     platinum: [],
     gold: [],
-    silver: [],
+    silver: [
+        {
+            name: "Lineage TD 21.0 (andyyan)",
+            link: "a14/andyyan_lineage_td_21_0"
+        }
+    ],
     bronze: [
         {
             name: "Treble Matrixx (ChonDoit)",
             link: "a14/chondoit_treble_matrixx"
-        }
+        },
     ],
     broken: []
 };
